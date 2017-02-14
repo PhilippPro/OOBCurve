a011adfbf9f3ee31c0c28e1e69efb380471a9246
PhilippPro <philipp_probst@gmx.de>
2017-02-14 11:53:15 +0100

    Added ranger to the package!

--- R/OOBCurve.R
+++ R/OOBCurve.R
@@ -14,12 +14,15 @@
 #'   Returns a dataframe with a column for each desired measure.
 #' @export
 #' @examples
-#' library(mlr)
+#' library(ranger)
 #' library(randomForest)
 #' 
 #' # Classification
 #' data = getTaskData(sonar.task)
-#' mod = randomForest(Class ~., data = data, ntree = 100, keep.inbag = TRUE)
+#' mod = ranger(Class ~., data = data, num.trees = 100, keep.inbag = TRUE)
+#' # Alternatively use randomForest
+#' # mod = randomForest(Class ~., data = data, ntree = 100, keep.inbag = TRUE)
+#' # Application of the main function
 #' results = OOBCurve(mod, measures = list(mmce, auc, brier), task = sonar.task, data = data)
 #' # Plot the generated results
 #' plot(results$mmce, type = "l", ylab = "oob-mmce", xlab = "ntrees")
@@ -28,13 +31,21 @@
 #' 
 #' # Regression
 #' data = getTaskData(bh.task)
-#' mod = randomForest(medv ~., data = data, ntree = 100, keep.inbag = TRUE, data = data)
-#' results = OOBCurve(mod, measures = list(mse, mae), task = bh.task)
-# Plot the generated results
+#' mod = ranger(medv ~., data = data, num.trees = 100, keep.inbag = TRUE)
+#' # Alternatively use randomForest
+#' # mod = randomForest(medv ~., data = data, ntree = 100, keep.inbag = TRUE)
+#' # Application of the main function
+#' results = OOBCurve(mod, measures = list(mse, mae), task = bh.task, data = data)
+#' # Plot the generated results
 #' plot(results$mse, type = "l", ylab = "oob-mse", xlab = "ntrees")
 #' plot(results$mae, type = "l", ylab = "oob-mae", xlab = "ntrees")
 #' 
 OOBCurve = function(mod, measures = list(auc), task, data) {
+  UseMethod("OOBCurve")
+}
+
+#' @export
+OOBCurve.randomForest.formula = function(mod, measures = list(auc), task, data) {
   tasktype = getTaskType(task)
   truth = mod$y
   preds = predict(mod, newdata = data, predict.all = TRUE)
@@ -57,6 +68,34 @@ OOBCurve = function(mod, measures = list(auc), task, data) {
   if (tasktype == "regr") {
     preds$individual = preds$individual * ((inbag == 0) * 1) # only use observations that are out of bag
     predis = rowCumsums(preds$individual) * (1 / rowCumsums((inbag == 0) * 1))
+    result = data.frame(t(apply(predis, 2, function(x) calculateMlrMeasure(x, measures, task, truth, predict.type = "response"))))
+  }
+  return(result)
+}
+
+#' @export
+OOBCurve.ranger = function(mod, measures = list(auc), task, data) {
+  tasktype = getTaskType(task)
+  truth = getTaskTargets(task)
+  preds = predict(mod, data = data, predict.all = TRUE)
+  inbag = do.call(cbind, mod$inbag.counts) 
+  
+  if (tasktype == "classif") {
+    ntree = mod$num.trees
+    nobs = length(mod$predictions)
+    num_levels = nlevels(truth)
+    pred_levels = levels(truth)
+    prob_array = array(data = NA, dim = c(nobs, ntree, num_levels), dimnames = list(NULL, NULL, pred_levels))
+    for(i in 1:length(pred_levels)) {
+      predis = (preds$predictions == i) * 1
+      predis = predis * ((inbag == 0) * 1) # only use observations that are out of bag
+      prob_array[, , i] = rowCumsums(predis) * (1 / rowCumsums((inbag == 0) * 1)) # divide by the number of observations that are out of bag
+    }
+    result = data.frame(t(apply(prob_array, 2, function(x) calculateMlrMeasure(x, measures, task, truth, predict.type = "prob"))))
+  }
+  if (tasktype == "regr") {
+    preds$predictions = preds$predictions * ((inbag == 0) * 1) # only use observations that are out of bag
+    predis = rowCumsums(preds$predictions) * (1 / rowCumsums((inbag == 0) * 1))
     result = data.frame(t(apply(predis, 2, function(x) calculateMlrMeasure(x, measures, task, truth, predict.type = "response"))))
   }
   return(result)
